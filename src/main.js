8c3620679052f0360634b00d2dcb5c04f2f30c77
Yago Coutto <62577739+YagoCoutto@users.noreply.github.com>
2024-01-28 16:26:23 -0300

    desktop

---
+++ src/main.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", (event) => {
+    const menuMobile = document.querySelector('#ckeckboxMenuMobile');
+    const menu = document.querySelector('#menu');
+    
+   /* menuMobile.addEventListener("click", () => {
+        if (menuMobile.checked) { 
+            menu.style.cssText = 'display: block;' 
+        }
+        if(!menuMobile.checked) menu.style.cssText = 'display: none;';
+
+        console.log(menuMobile.checked)
+    })*/
+})
